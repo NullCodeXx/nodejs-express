b422cca1a7ab093402c6866b00c86e7c23f9011d
Djaafar Boumaza <26055564+djaafarcode@users.noreply.github.com>
2017-08-21 12:17:54 +0200

    frst commit || servir des fichier statique module express du serveur nodejs

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+//doc  Servir des fichiers statiques dans Express: http://expressjs.com/fr/starter/static-files.html
+
+//Hello word.
+var express = require('express');
+var app = express();
+
+// REGLE ATTENTION : Le premier l'emporte (app).
+
+app.listen(3000, function () {
+  console.log("L'application ecoute le port 3000.");
+});
+
+//servir un le fichier index.html dans le dossier public.
+//app.use(express.static('public'));
+
+// utilise dans l'abrescence du dossier public. || http://localhost:3000/tata/plouc/
+app.use("/tata/plouc/", express.static('public'));
+
+//Récupère l'argument de la function envoyer.
+// app.get('/', function (req, res) {
+//   res.send('Salut tout le monde!');
+// });
+
